db13fecc17e26e1372db5aa65ff1f92e6f8ff865
Pranav Gade <pranavgade20@gmail.com>
2021-10-25 21:52:02 +0530

    week 8
    not using preview witch statements in first question

--- src/main/java/week8/_1/ObjInfo.java
+++ src/main/java/week8/_1/ObjInfo.java
@@ -12,25 +12,14 @@ public class ObjInfo<T> {
     }
 
     public void printInfo() {
-        switch (collection) {
-            case Collection<?> c -> {
-                System.out.println("Size is: " + c.size());
-                System.out.println("Contents are: ");
-                c.forEach(System.out::println);
-            }
-            case Map<?, ?> m -> {
-                System.out.println("Size is: " + m.size());
-                System.out.println("Contents are: ");
-                m.forEach((k, v) -> System.out.println(k + ": " + v));
-            }
-            default -> System.out.println("Collection not supported");
-        }
         if (collection instanceof Collection<?>) {
             System.out.println("Size of collection is: " + ((Collection<?>)collection).size());
             System.out.println("Collection contains: ");
             ((Collection<?>)collection).forEach(System.out::println);
         } else if (collection instanceof Map<?,?>) {
-            System.out.println(collection);
+            System.out.println("Size is: " + ((Map<?,?>)collection).size());
+            System.out.println("Contents are: ");
+            ((Map<?,?>)collection).forEach((k, v) -> System.out.println(k + ": " + v));
         }
     }
 }
